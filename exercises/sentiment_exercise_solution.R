548dc8afc933ecde12478664fd7c31d222ec187f
Mustafa Asım Ruhi <mustafaasimruhi@gmail.com>
2026-05-31 21:53:23 +0200

    Clean repository for final submission

--- exercises/sentiment_exercise_solution.R
+++
@@ -1,4 +0,0 @@
-# Solution
-# Correct answer: B
-# The negative words point to delayed flights, terrible service, lost luggage, and lack of help.
-# A reasonable managerial action is to improve delay communication and luggage recovery processes.
